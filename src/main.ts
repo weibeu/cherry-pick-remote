4108151d72ad256ca5886000f74dc5b5ca0e26a2
weibeu <deepakrajko14@gmail.com>
2021-06-11 17:37:00 +0530

    Implement the action workflow in Javascript

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,50 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import { exec } from '@actions/exec'
+
+const REMOTE_REPOSITORY_TAG = "remote-repository"
+
+async function exec_(command: String): Promise<void> {
+  var _ = command.split(" ")
+  await exec(_[0], _.slice(1))
+}
 
 async function run(): Promise<void> {
+
+  if (github.context.payload.action !== "push-dispatch") {
+    core.setFailed("Received GitHub event which is not push-dispatch.")
+    return
+  }
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    var repository = core.getInput("repository")
+    var branch = core.getInput("branch")
+    var path = core.getInput("path")
+    var token = core.getInput("token")
+    var username = core.getInput("username")
+    var email = core.getInput("email")
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(`Checking out to ${branch} branch.`)
+    core.setOutput("ref", branch)
+    core.setOutput("fetch-depth", 0)
+    var _checkout = require("checkout/dist/index.js")
 
-    core.setOutput('time', new Date().toTimeString())
+    core.info("Going to setup the GitHub credentials.")
+    core.setOutput("credentials", token)
+    var _setupGitCredentials = require("setup-git-credentials/lib/main.js")
+
+    core.info("Going to configure Git user details.")
+    await exec_(`git config user.name "${username}"`)
+    await exec(`git config user.email "${email}"`)
+
+    core.info("Cherry pick commits from source repository.")
+    await exec_(`git remote add ${REMOTE_REPOSITORY_TAG} https://github.com/${repository}`)
+    await exec_(`git fetch ${REMOTE_REPOSITORY_TAG} --force`)
+    await exec_(`git cherry-pick -x ${ github.context.payload.client_payload.before }..${ github.context.payload.client_payload.after }`)
+
+    core.info(`Push updated ref to ${branch} branch`)
+    await exec_(`git push --force`)
+
   } catch (error) {
     core.setFailed(error.message)
   }
